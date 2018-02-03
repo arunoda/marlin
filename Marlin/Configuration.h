e84f25a9e0f881d85ad17202acf9e928d4e40a20
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2018-02-04 03:27:25 +0530

    Set bed levelling to 9 points.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -910,7 +910,7 @@
 #if ENABLED(AUTO_BED_LEVELING_LINEAR) || ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 2
+  #define GRID_MAX_POINTS_X 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
